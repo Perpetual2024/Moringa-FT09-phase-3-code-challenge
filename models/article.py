dfdd4dfee72b03f8e56429537bac68bf154f2b45
Perpetual Akinyi <you@example.com>
2024-12-15 17:51:42 +0300

    finished setting up the db connection

--- models/article.py
+++ models/article.py
@@ -1,13 +1,13 @@
-
+from database.connection import get_db_connection
 
 
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
-        self.id = id
-        self.title = title
-        self.content = content
-        self.author_id = author_id
-        self.magazine_id = magazine_id
+        self._idid = id
+        self._title = title
+        self._content = content
+        self._author_id = author_id
+        self._magazine_id = magazine_id
 
         if not isinstance(title, str):
           raise TypeError("Title must be a string")
@@ -15,9 +15,15 @@ class Article:
             raise TypeError("Title must be between 5 and 50 characterslong")
         
         if not isinstance(content, str):
-            raise TypeError("Content must be a string")
+            raise TypeError("Content must be a string.")
         if len(content) == 0:
-            raise TypeError("Content must not be empty")
+            raise TypeError("Content must not be empty.")
+        
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO articles (title, content, author_id, magazine_id) VALUES (?, ?, ? ,?)", (title, content, author_id, magazine_id))
+        conn.commit()
+        conn.close()
         
 
 
