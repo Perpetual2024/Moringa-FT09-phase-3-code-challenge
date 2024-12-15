0f80e26ee486dc19dc6d08e4a056bac295070d19
Perpetual Akinyi <you@example.com>
2024-12-15 09:27:46 +0300

    Validating the name entry

--- models/author.py
+++ models/author.py
@@ -1,7 +1,12 @@
 class Author:
     def __init__(self, id, name):
+
         self.id = id
         self.name = name
 
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string")
+
     def __repr__(self):
         return f'<Author {self.name}>'
+   
