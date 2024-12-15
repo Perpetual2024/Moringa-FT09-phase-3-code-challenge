5d50ab021506699db173de24284686a88dcf4793
Perpetual Akinyi <you@example.com>
2024-12-16 00:17:53 +0300

    commit all changes

--- models/article.py
+++ models/article.py
@@ -1,49 +1,103 @@
 from database.connection import get_db_connection
 
-
 class Article:
     def __init__(self, id, title, content, author_id, magazine_id):
-        self._idid = id
-        self._title = title
-        self._content = content
-        self._author_id = author_id
-        self._magazine_id = magazine_id
-
-        if not isinstance(title, str):
-          raise TypeError("Title must be a string")
-        if not (5 <= len(title) <= 50):
-            raise TypeError("Title must be between 5 and 50 characterslong")
-        
-        if not isinstance(content, str):
-            raise TypeError("Content must be a string.")
-        if len(content) == 0:
-            raise TypeError("Content must not be empty.")
-        
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO articles (title, content, author_id, magazine_id) VALUES (?, ?, ? ,?)", (title, content, author_id, magazine_id))
-        conn.commit()
-        conn.close()
-
-    @property
-    def id(self):
-        return self._id
-    
-    @property
-    def title(self):
-        return self._title
-    @title.setter
-    def title(self, value):
-        raise AttributeError("Title cannot be changed.")
-
-    @property
-    def content(self):
-        return self._content
-    @content.setter
-    def content(self, value):
-        raise AttributeError("Content cannot be changed.")
-
-
+        self.id = id
+        self.title = title if title else "Untitled"
+        self.content = content
+        self.author_id = author_id
+        self.magazine_id = magazine_id
 
     def __repr__(self):
         return f'<Article {self.title}>'
+    
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if hasattr(self, '_title'):
+            raise AttributeError("Cannot change title after it has been set")
+        if isinstance(title, str) and len(title):
+            self._title = title
+        else:
+            raise ValueError(
+                "Title must be a non-empty string"
+            )
+        
+    @property
+    def content(self):
+        return self._content
+    
+    @content.setter
+    def content(self, content):
+        if isinstance(content, str) and len(content):
+            self._content = content
+        else:
+            raise ValueError(
+                "Content must be a non-empty string"
+            )
+        
+    @property
+    def author_id(self):
+        return self._author_id
+    
+    @author_id.setter
+    def author_id(self, author_id):
+        from models.author import Author
+        if type(author_id) is int and Author.find_by_id(author_id):
+            self._author_id = author_id
+        else:
+            raise ValueError(
+                "Author ID must reference an author in the database")
+        
+    @property
+    def magazine_id(self):
+        return self._magazine_id
+    
+    @magazine_id.setter
+    def magazine_id(self, magazine_id):
+        from models.magazine import Magazine
+        if type(magazine_id) is int and Magazine.find_by_id(magazine_id):
+            self._magazine_id = magazine_id
+        else:
+            raise ValueError("Magazine ID must reference a magazine in the database")
+    def author(self):
+        from models.author import Author
+        conn = get_db_connection()
+        CURSOR = conn.cursor()
+        """retrieves and returns the author who wrote this article"""
+        sql = """
+            SELECT a.*
+            FROM authors a
+            INNER JOIN articles ar ON ar.author = a.id
+            WHERE ar.id = ?
+        """
+        CURSOR.execute(sql, (self.id))
+        author_data = CURSOR.fetchone()
+
+        if author_data:
+            return Author(*author_data)
+        else:
+            return None
+
+    def magazine(self):
+        from models.magazine import Magazine
+        conn = get_db_connection()
+        CURSOR = conn.cursor()
+        """retrieves and returns the magazine in which this article is published"""
+        sql = """
+            SELECT m.*
+            FROM magazines m
+            INNER JOIN articles ar ON ar.magazine = m.id
+            WHERE ar.id = ?
+        """
+        CURSOR.execute(sql, (self.id))
+        magazine_data = CURSOR.fetchone()
+
+        if magazine_data:
+            return Magazine(*magazine_data)
+        else:
+            return None
--- models/author.py
+++ models/author.py
@@ -1,62 +1,152 @@
+# /models/Author.py
 from database.connection import get_db_connection
 
-
-
 class Author:
+    all = {}
+
     def __init__(self, id, name):
+        self.id = id
+        self.name = name
 
-        self._id = cursor.lastowid
-        self._name = name
+    def __repr__(self):
+        return f'<Author {self.id} {self.name}>'
+    
 
-        if not isinstance(name, str):
-            raise TypeError("Name must be a string")
-        if len(name) == 0:
-            raise ValueError("Name must be longer than 0 characters")
-        
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO authors (name) VALUES (?, ?)", (name))
-        conn.commit()
-        conn.close()
+    @property
+    def id(self):
+        return self._id
+    
+    @id.setter
+    def id(self, id):
+        if isinstance(id, int):
+            self._id = id
+
 
     @property
     def name(self):
         return self._name
-
+    
     @name.setter
-    def name(self, value):
-        raise AttributeError("Name cannot be changed")
+    def name(self, new_name):
+        if hasattr(self, '_name'):
+            raise AttributeError("Name cannot be changed after initializING")
+        else:
+            if isinstance(new_name, str):
+                if len(new_name) > 0:
+                    self._name = new_name
 
-    @property
-    def id(self):
-        if not isinstance(self._id, int):
-            raise TypeError("ID must be of type int.")
-        return self._id
 
-
-    @id.setter
-    def id(self, value):
-        raise AttributeError("ID cannot be changed")
-    
-    def article(self):
+    def save(self):
         conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM articles WHERE author_id = ?", (self.id,))
-        articles = cursor.fetchall()
-        conn.close()
-        return [dict (article) for article in articles]
-    
-    def magazines(self):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM magazines WHERE author_id = ?", (self.id,))
-        magazines = cursor.fetchall()
-        conn.close()
-        return [dict (magazine) for magazine in magazines]
+        CURSOR = conn.cursor()
+        sql = """
+            INSERT INTO authors (name)
+            VALUES (?)
+        """
+        CURSOR.execute(sql, (self.name,))
+        conn.commit()
         
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
 
+    @classmethod
+    #creates a new entry in the authors table of the database
+    def create(cls, name):
+        author = cls(name)
+        author.save()
+        return author
+    
+    def get_author_id(self):
+        return self.id
 
+    def articles(self):
+        from models.article import Article
+        conn = get_db_connection()
+        CURSOR = conn.cursor()
+        """retrieves and returns a list of articles wriitten by this author"""
+        sql = """
+            SELECT ar.*
+            FROM articles ar
+            INNER JOIN authors a ON ar.author = a.id
+            WHERE a.id = ?
+        """
 
-    def __repr__(self):
-        return f'<Author {self.name}>'
-   
+        CURSOR.execute(sql, (self.id,))
+        article_data = CURSOR.fetchall()
+
+        articles = []
+        for row in article_data:
+            articles.append(Article(*row))
+        return articles
+    
+
+    def magazines(self):
+        from models.magazine import Magazine
+        conn = get_db_connection()
+        CURSOR = conn.cursor()
+        """Retrieves and returns a list of Magazine objects where this Author has written articles"""
+        sql = """
+            SELECT DISTINCT m.*
+            FROM magazines m
+            INNER JOIN articles ar ON ar.magazine = m.id
+            INNER JOIN authors a ON ar.author = a.id
+            WHERE a.id = ?
+        """
+
+        CURSOR.execute(sql, (self.id,))
+        magazine_data = CURSOR.fetchall()
+
+        magazines = []
+        for row in magazine_data:
+            magazines.append(Magazine(*row))
+        return magazines
+    
+    def update(self):
+        conn = get_db_connection()
+        """Update the table row corresponding to the current Article instance."""
+        sql = """
+            UPDATE authors
+            SET author_id = ?, name = ?
+            WHERE id =?
+        """
+        cur = conn.cursor()
+        cur.execute(sql, (self.author_id, self.name))
+        conn.commit()
+
+    def delete(self):
+        conn = get_db_connection()
+        """Delete the table row corresponding to the current Article instance,
+        delete the dictionary entry, and reassign id attribute"""
+
+        sql = """
+            DELETE FROM authors
+            WHERE id =?
+        """
+
+        cur = conn.cursor()
+        cur.execute(sql, (self.author_id,))
+        conn.commit()
+
+        # Delete the dictionary entry using id as the key
+        del type(self).all[self.author_id]
+
+        # Set the id to None
+        self.id = None
+
+    @classmethod
+    def find_by_id(cls, author_id):
+        """Find an author by id"""
+        conn = get_db_connection()
+        CURSOR = conn.cursor()
+        sql = """
+            SELECT *
+            FROM authors
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (author_id,))
+        author_data = CURSOR.fetchone()
+
+        if author_data:
+            return cls(*author_data)
+        else:
+            return None
