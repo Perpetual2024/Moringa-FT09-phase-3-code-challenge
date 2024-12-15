470dcf3fc537e3ad7992b95d1f227828b0704767
Perpetual Akinyi <you@example.com>
2024-12-15 09:32:26 +0300

    making a getter and setter for the name instance

--- models/author.py
+++ models/author.py
@@ -6,6 +6,13 @@ class Author:
 
         if not isinstance(name, str):
             raise TypeError("Name must be a string")
+        if len(name) == 0:
+            raise ValueError("Name must be longer than 0 characters")
+
+    @property
+    def name(self):
+        return self._name
+
 
     def __repr__(self):
         return f'<Author {self.name}>'
